effc577bc8c3b9a63c38310ce932765d4ac4824f
Paolo Maria Guardiani <paolomariaguardiani@yahoo.it>
2021-02-18 20:23:11 +0100

    Initial commit My first commit

---
+++ app/src/main/java/com/paolomariaguardiani/a26gitandgithybinandroidvcs/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.paolomariaguardiani.a26gitandgithybinandroidvcs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Our first Project on Github
+    }
+}
